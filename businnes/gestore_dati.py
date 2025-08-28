e5b36ad9434ce07f920cd9862f8c8a4fa77a2f2b
SuperJack2003 <80111456+SuperJack2003@users.noreply.github.com>
2025-08-28 11:57:46 +0200

    Commit: implementation of metod carica_dati in GestoreDati

--- businnes/gestore_dati.py
+++ businnes/gestore_dati.py
@@ -3,7 +3,15 @@ from pathlib import Path
 
 from domain.attività.atleta import Atleta
 from domain.attività.allenatore import Allenatore
+from domain.attività.utente import Utente
 
+from domain.servizio.abbonamento import Abbonamento
+from domain.servizio.corso import Corso
+from domain.servizio.scheda import Scheda
+from domain.servizio.esercizio import Esercizio
+from domain.servizio.notifica import Notifica
+
+from domain.attività.contratto import Contratto
 from domain.attività.contratto_atleta_corso import ContrattoAtletaCorso
 from domain.attività.contratto_scheda import ContrattoScheda
 from domain.attività.contratto_esercizio import ContrattoEsercizio
@@ -28,15 +36,8 @@ def carica_dati(gestore_atleti: GestoreAtleti, gestore_allenatori: GestoreAllena
             print ("Caricamento utenti...")
 
             lista_utenti = pickle.load(f)
-            lista_atleti = []
-            lista_allenatori = []
-
-            for utente in lista_utenti:
-                if isinstance(utente, Atleta):
-                    lista_atleti.append(utente)
-
-                elif isinstance(utente, Allenatore):
-                    lista_allenatori.append(utente)
+            lista_atleti = lista_utenti["atleti"]
+            lista_allenatori = lista_utenti["allenatori"]
 
             #Caricamento Utenti
             gestore_atleti.set_lista_atleti(lista_atleti)
@@ -78,31 +79,12 @@ def carica_dati(gestore_atleti: GestoreAtleti, gestore_allenatori: GestoreAllena
             print("Caricamento contratti...")
 
             lista_contratti = pickle.load(f)
-            lista_contratti_abbonamento = []
-            lista_contratti_scheda = []
-            lista_contratti_esercizi = []
-            lista_contratti_allenatore_corso = []
-            lista_contratti_atleta_corso = []
-            lista_contratti_allenatore_atleta = []
-
-            for contratto in lista_contratti:
-                if isinstance(contratto, ContrattoAbbonamento):
-                    lista_contratti_abbonamento.append(contratto)
-
-                elif isinstance(contratto, ContrattoScheda):
-                    lista_contratti_scheda.append(contratto)
-
-                elif isinstance(contratto, ContrattoEsercizio):
-                    lista_contratti_esercizi.append(contratto)
-
-                elif isinstance(contratto, ContrattoAllenatoreCorso):
-                    lista_contratti_allenatore_corso.append(contratto)
-
-                elif isinstance(contratto, ContrattoAtletaCorso):
-                    lista_contratti_atleta_corso.append(contratto)
-
-                elif isinstance(contratto, ContrattoAtletaAllenatore):
-                    lista_contratti_allenatore_atleta.append(contratto)
+            lista_contratti_abbonamento = lista_contratti["contratti_abbonamento"]
+            lista_contratti_scheda = lista_contratti["contratti_scheda"]
+            lista_contratti_esercizi = lista_contratti["contratti_esercizi"]
+            lista_contratti_allenatore_corso = lista_contratti["contratti_allenatore_corso"]
+            lista_contratti_atleta_corso = lista_contratti["contratti_atleta_corso"]
+            lista_contratti_allenatore_atleta = lista_contratti["contratti_allenatore_atleta"]
 
             #Caricamento Contratti Allenatori
             gestore_allenatori.set_lista_contratti(lista_contratti_allenatore_atleta)
@@ -124,7 +106,78 @@ def carica_dati(gestore_atleti: GestoreAtleti, gestore_allenatori: GestoreAllena
             lista_notifiche = pickle.load(f)
 
             #Caricamento Notifiche
-            gestore_notifiche.set_lista_notifiche(lista_notifiche, gestore_atleti, gestore_allenatori)
+            gestore_notifiche.set_lista_notifiche(lista_notifiche)
 
     except FileNotFoundError:
-        print(f"File {f}")
+        print(f"File {f} non trovato")
+
+def salva_dati(gestore_atleti: GestoreAtleti, gestore_allenatori: GestoreAllenatori, gestore_abbonamenti: GestoreAbbonamenti,
+                gestore_corsi: GestoreCorsi, gestore_schede: GestoreSchede, gestore_notifiche: GestoreNotifiche):
+
+    try:
+        with open(cartella_dati / 'utenti.pkl', 'wb') as f:
+            print ("Salvataggio utenti...")
+
+            lista_atleti = gestore_atleti.get_lista_atleti()
+            lista_allenatori = gestore_allenatori.get_lista_allenatori()
+
+            lista_utenti = {
+                "atleti": lista_atleti,
+                "allenatori": lista_allenatori,
+            }
+
+            pickle.dump(lista_utenti, f)
+
+        with open(cartella_dati / 'abbonamenti.pkl', 'wb') as f:
+            print ("Salvataggio abbonamenti...")
+
+            lista_abbonamenti = gestore_abbonamenti.get_lista_abbonamenti()
+            pickle.dump(lista_abbonamenti, f)
+
+        with open(cartella_dati / 'corsi.pkl', 'wb') as f:
+            print ("Salvataggio corsi...")
+
+            lista_corsi = gestore_corsi.get_lista_corsi()
+            pickle.dump(lista_corsi, f)
+
+        with open(cartella_dati / 'schede.pkl', 'wb') as f:
+            print ("Salvataggio schede...")
+
+            lista_schede = gestore_schede.get_lista_schede()
+            pickle.dump(lista_schede, f)
+
+        with open(cartella_dati / 'esercizi.pkl', 'wb') as f:
+            print ("Salvataggio esercizi...")
+
+            lista_esercizi = gestore_schede.get_lista_esercizi()
+            pickle.dump(lista_esercizi, f)
+
+        with open(cartella_dati / 'contratti.pkl', 'wb') as f:
+            print ("Salvataggio contratti...")
+
+            lista_contratti_abbonamento = gestore_abbonamenti.get_lista_contratti()
+            lista_contratti_scheda = gestore_schede.get_lista_contratti_schede()
+            lista_contratti_esercizi = gestore_schede.get_lista_contratti_esercizi()
+            lista_contratti_allenatore_corso = gestore_corsi.get_lista_contratti_allenatori()
+            lista_contratti_atleta_corso = gestore_corsi.get_lista_contratti_atleti()
+            lista_contratti_allenatore_atleta = gestore_allenatori.get_lista_contratti()
+
+            lista_contratti = {
+                "contratti_abbonamento": lista_contratti_abbonamento,
+                "contratti_scheda": lista_contratti_scheda,
+                "contratti_esercizi": lista_contratti_esercizi,
+                "contratti_allenatore_corso": lista_contratti_allenatore_corso,
+                "contratti_atleta_corso": lista_contratti_atleta_corso,
+                "contratti_allenatore_atleta": lista_contratti_allenatore_atleta,
+            }
+
+            pickle.dump(lista_contratti, f)
+
+        with open(cartella_dati / 'notifiche.pkl', 'wb') as f:
+            print("Salvataggio notifiche...")
+
+            lista_notifiche = gestore_notifiche.get_lista_notifiche()
+            pickle.dump(lista_notifiche, f)
+
+    except FileNotFoundError:
+        print(f"File {f} non trovato")
